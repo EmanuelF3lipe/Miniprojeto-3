58736197191d1a695fa2575f8c07b39fe900b5c5
EmanuelF3lipe <emanuel.grepo@gmail.com>
2024-04-30 21:27:33 -0300

    teste1

---
+++ src/Capitulo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Capitulo {
+    String nome;
+    String texto;
+    String escolha1;
+    String escolha2;
+    Personagem personagem;
+    int alteracaoEnergia;
+    Scanner escaneador;
+}
---
+++ src/Miniprojeto3.java
@@ -0,0 +1,6 @@
+
+public class Miniprojeto3 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
